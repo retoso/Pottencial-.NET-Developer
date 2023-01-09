4581212a75e13a173075a839a77b7d2c26ec238f
Renato Tonelli de Souza <renato.souza1969@yahoo.com.br>
2023-01-09 12:10:08 -0300

    treinamento no dotnet

--- Program.cs
+++ Program.cs
@@ -1,8 +1,29 @@
 ﻿using FundamentoPessoa.Models;
 
-Pessoa p = new Pessoa();
+int quantidadeEmEstoque = 3;
 
-p.Nome = "Renato";
-p.Idade = 53;
-p.Apresentar();
+int quantidadeCompra = 4;
+
+bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+
+
+
+Console.WriteLine($"Quantidade em Estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade na compra: {quantidadeCompra}");
+Console.WriteLine($"E possivel realizar a venda?: {possivelVenda}");
+
+if (quantidadeCompra == 0)
+{
+    Console.WriteLine("Venda Invalida");
+}
+else if (possivelVenda)
+{
+    Console.WriteLine("Venda Realizada");
+}
+else
+{
+    Console.WriteLine("Desculpe, nao temos a quantidade desejada em estoque");
+
+}
+
 
